ea00a9b81581ae3a0a9d8e626c5b59495c5ff259
Leonardo Felipe <leonardo.fpsilva@hotmail.com>
2020-08-24 23:45:35 -0300

    Start a repository

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyle from './styles/GlobalStyle';
+import Layout from './components/Layout';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Layout />
+    </>
+  );
+};
+
+export default App;
