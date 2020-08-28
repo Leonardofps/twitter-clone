e616dea947329168faf2e699a1d4808537b2ed64
Leonardo Felipe <leonardo.fpsilva@hotmail.com>
2020-08-27 23:42:58 -0300

    Add a new components like the menu and trend-topics

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -7,6 +7,10 @@ export {
   LocationOn,
   Search,
 } from 'styled-icons/material-outlined';
+
+export { RocketseatIcon as Rocketseat } from '../components/RocketseatIcon';
 export { Person, Cake, Favorite } from 'styled-icons/material';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
+export { Retweet } from 'styled-icons/fa-solid';
 export { Chat } from 'styled-icons/heroicons-outline';
+export { TwitterOutline as Twitter } from 'styled-icons/evaicons-outline';
